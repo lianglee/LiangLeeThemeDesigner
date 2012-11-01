0aaeaae5a58de3e49e5575243e3b11286b2a04cf
unknown <manactric@gmail.com>
2012-11-01 15:55:30 +0500

    get search color

---
+++ views/default/LiangLeeThemeDesigner/colors/srch.php
@@ -0,0 +1,7 @@
+ <script type="text/javascript" charset="utf-8">
+  $(document).ready(function() {
+    $('#srch~').hide();
+    $('#srchp').farbtastic('#srchc');
+  });
+ </script>
+<div id="srchp"></div>
