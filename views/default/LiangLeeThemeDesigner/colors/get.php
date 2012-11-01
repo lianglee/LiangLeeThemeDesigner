166a45791392c0825ce58e7ad5c407b138ebd80f
unknown <manactric@gmail.com>
2012-11-01 15:53:17 +0500

    changes in header color get

--- views/default/LiangLeeThemeDesigner/colors/get.php
+++ views/default/LiangLeeThemeDesigner/colors/get.php
@@ -1,9 +1,7 @@
  <script type="text/javascript" charset="utf-8">
   $(document).ready(function() {
-    $('#demo').hide();
+    $('#headec~').hide();
     $('#picker').farbtastic('#color');
   });
  </script>
-<form action="" style="width: 400px;">
-  <div class="form-item"><label for="color">Color:</label><input type="text" id="color" name="color" value="#123456" /></div><div id="picker"></div>
-</form>
+<div id="picker"></div>
