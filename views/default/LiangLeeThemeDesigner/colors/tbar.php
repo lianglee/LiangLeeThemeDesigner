027617c1c35e4c80c535f3570ec55e968c31cae8
unknown <manactric@gmail.com>
2012-11-01 15:55:46 +0500

    get topbar color

---
+++ views/default/LiangLeeThemeDesigner/colors/tbar.php
@@ -0,0 +1,7 @@
+ <script type="text/javascript" charset="utf-8">
+  $(document).ready(function() {
+    $('#tbar~').hide();
+    $('#tbarp').farbtastic('#tbarc');
+  });
+ </script>
+<div id="tbarp"></div>
