0d4a7b06e256e09a0a28514975f10dc5c2e84b49
unknown <manactric@gmail.com>
2012-11-04 09:50:28 +0500

    fix color selector for header

---
+++ views/default/LiangLeeThemeDesigner/colors/header.php
@@ -0,0 +1,7 @@
+ <script type="text/javascript" charset="utf-8">
+  $(document).ready(function() {
+    $('#body~').hide();
+    $('#bodyp').farbtastic('#bodyc');
+  });
+ </script>
+<div id="bodyp"></div>
